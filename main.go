f8a3aeaffe9e90a1fedcac9bd9d3dfbc4c20e534
ohbeomho <ohbeomho0322@gmail.com>
2026-08-28 16:35:57 +0900

    binary tree complete maybe
    I'll fix it later if it has bugs

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+)
+
 type Node struct {
-	key int
-	left, right *Node
+	key                 int
+	left, right, parent *Node
 }
 
 type BinaryTree struct {
@@ -15,16 +21,26 @@ func (tree *BinaryTree) insert(key int) {
 		return
 	}
 
-	curr := &tree.root
-	for *curr != nil {
-		if key > (*curr).key {
-			curr = &(*curr).right
+	curr := tree.root
+	var prev *Node
+	for curr != nil {
+		prev = curr
+
+		if key > curr.key {
+			curr = curr.right
 		} else {
-			curr = &(*curr).left
+			curr = curr.left
 		}
 	}
 
-	*curr = &Node{key: key}
+	newNode := &Node{key: key}
+	if key > prev.key {
+		prev.right = newNode
+	} else {
+		prev.left = newNode
+	}
+
+	newNode.parent = prev
 }
 
 func (tree BinaryTree) search(key int) (bool, []*Node) {
@@ -48,10 +64,88 @@ func (tree BinaryTree) search(key int) (bool, []*Node) {
 	return true, path
 }
 
+func (tree BinaryTree) getSuccessor(node *Node) *Node {
+	if node.right == nil {
+		return nil
+	}
+
+	curr := node.right
+	for curr.left != nil {
+		curr = curr.left
+	}
+
+	return curr
+}
+
 func (tree *BinaryTree) remove(node *Node) {
-	// TODO: Implement it
+	successor := tree.getSuccessor(node)
+
+	if successor == nil {
+		if node.left == nil {
+			if node.key > node.parent.key {
+				node.parent.right = nil
+			} else {
+				node.parent.left = nil
+			}
+
+			return
+		}
+
+		node.key = node.left.key
+
+		node.left = node.left.left
+		if node.left != nil {
+			node.left.parent = node
+		}
+
+		node.right = node.left.right
+		if node.right != nil {
+			node.right.parent = node
+		}
+
+		return
+	}
+
+	node.key = successor.key
+
+	if successor.key > successor.parent.key {
+		successor.parent.right = successor.right
+	} else {
+		successor.parent.left = successor.right
+	}
+
+	if successor.right != nil {
+		successor.right.parent = successor.parent
+	}
+}
+
+func printTree(node *Node, level int) {
+	if node == nil {
+		return
+	}
+
+	fmt.Printf("%s%d\n", strings.Repeat("-", level), node.key)
+	printTree(node.left, level+1)
+	printTree(node.right, level+1)
 }
 
 func main() {
 	tree := BinaryTree{}
+
+	for range 20 {
+		tree.insert(rand.Intn(100))
+	}
+
+	printTree(tree.root, 0)
+
+	var target int
+	fmt.Scan(&target)
+
+	found, path := tree.search(target)
+
+	if found {
+		tree.remove(path[len(path)-1])
+
+		printTree(tree.root, 0)
+	}
 }
