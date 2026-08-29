8ac113af3711191acf2fc5c9045479bf2f16f504
ohbeomho <ohbeomho0322@gmail.com>
2026-08-29 15:04:57 +0900

    changed tree to array of int

--- main.go
+++ main.go
@@ -2,140 +2,80 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"strings"
+	"unsafe"
 )
 
-type Node struct {
-	key                 int
-	left, right, parent *Node
+// root = 1
+// left child = index * 2
+// right child = index * 2 + 1
+var tree []int32
+var initialized = false
+
+// Figure out how to return array
+
+//go:wasmexport treeInit
+func initTree(levels int32) unsafe.Pointer {
+	var treeSize int32
+
+	for i := range levels + 1 {
+		treeSize += 2 ^ i
+	}
+
+	tree = make([]int32, treeSize+1)
+
+	initialized = true
+	return unsafe.Pointer(&tree[0])
 }
 
-type BinaryTree struct {
-	root *Node
-}
-
-func (tree *BinaryTree) insert(key int) {
-	if tree.root == nil {
-		tree.root = &Node{key: key}
+//go:wasmexport treeInsert
+func insert(key int32) {
+	if !initialized {
 		return
 	}
 
-	curr := tree.root
-	var prev *Node
-	for curr != nil {
-		prev = curr
+	// 0을 빈 값으로 사용하기 위함
+	if key == 0 {
+		return
+	}
 
-		if key > curr.key {
-			curr = curr.right
+	curr := 1
+	for tree[curr] != 0 {
+		if key > tree[curr] {
+			curr = curr*2 + 1
 		} else {
-			curr = curr.left
+			curr = curr * 2
 		}
 	}
 
-	newNode := &Node{key: key}
-	if key > prev.key {
-		prev.right = newNode
-	} else {
-		prev.left = newNode
-	}
-
-	newNode.parent = prev
+	tree[curr] = key
 }
 
-func (tree BinaryTree) search(key int) (bool, []*Node) {
-	curr := tree.root
-	path := []*Node{curr}
+//go:wasmexport treeSearch
+func search(key int32) int32 {
+	if !initialized {
+		return 0
+	}
 
-	for curr != nil && curr.key != key {
-		if key > curr.key {
-			curr = curr.right
-		} else if key < curr.key {
-			curr = curr.left
+	if key == 0 {
+		return 0
+	}
+
+	var curr int32 = 1
+	for tree[curr] != 0 && tree[curr] != key {
+		if key > tree[curr] {
+			curr = curr*2 + 1
+		} else {
+			curr = curr * 2
 		}
-
-		path = append(path, curr)
 	}
 
-	if curr == nil {
-		return false, path
-	}
-
-	return true, path
-}
-
-func (tree BinaryTree) getSuccessor(node *Node) *Node {
-	if node.right == nil {
-		return nil
-	}
-
-	curr := node.right
-	for curr.left != nil {
-		curr = curr.left
+	if tree[curr] == 0 {
+		return 0
 	}
 
 	return curr
 }
 
-// TODO: Change it for visualization like insert function
-func (tree *BinaryTree) remove(node *Node) {
-	successor := tree.getSuccessor(node)
-
-	if successor == nil {
-		if node.left == nil {
-			if node.key > node.parent.key {
-				node.parent.right = nil
-			} else {
-				node.parent.left = nil
-			}
-
-			return
-		}
-
-		node.key = node.left.key
-
-		node.left = node.left.left
-		if node.left != nil {
-			node.left.parent = node
-		}
-
-		node.right = node.left.right
-		if node.right != nil {
-			node.right.parent = node
-		}
-
-		return
-	}
-
-	node.key = successor.key
-
-	if successor.key > successor.parent.key {
-		successor.parent.right = successor.right
-	} else {
-		successor.parent.left = successor.right
-	}
-
-	if successor.right != nil {
-		successor.right.parent = successor.parent
-	}
-}
-
-func printTree(node *Node, level int) {
-	if node == nil {
-		return
-	}
-
-	fmt.Printf("%s%d\n", strings.Repeat("-", level), node.key)
-	printTree(node.left, level+1)
-	printTree(node.right, level+1)
-}
-
 func main() {
-	tree := BinaryTree{}
-
-	for range 20 {
-		tree.insert(rand.Intn(100))
-	}
-
-	printTree(tree.root, 0)
+	fmt.Println("test")
 }
